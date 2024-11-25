09d109906de62822c0ad76976cba39be2d72b2ed
José Neto <140574691+jose-pcn@users.noreply.github.com>
2024-11-25 12:17:50 -0300

    Commit Teste GitHub

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import flet as ft
+from assets import cores
 
 def main(page: ft.Page):
     page.title = "Método dos Elementos Finitos"
     page.window.width = 950
     page.window.height = 700
     page.window.resizable = False
-    page.bgcolor = "#001b40"
+    page.window.maximizable = False
+    page.bgcolor = cores.AZUL_MARINHO_ESCURO
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    page.window.center()
+
+    # Registre a fonte antes de aplicá-la no tema
+    page.fonts = {
+        "Roboto_regular": "./assets/fonts/Roboto-Regular.ttf",
+        "Roboto_black": "./assets/fonts/Roboto-Black.ttf"
+    }
 
     # Aplicando a fonte Roboto em toda a página
-    page.font_family = "Roboto"
+    page.theme = ft.Theme(font_family="Roboto_regular")
 
     # Adicionando a logo
     imagem_logo = ft.Image(
@@ -20,20 +31,21 @@ def main(page: ft.Page):
 
     # Retângulo (container) do menu
     rectangle_menu = ft.Container(
-        width=475,
+        width=450,
         height=630,
         bgcolor="white",
         border_radius=20,
-        alignment=ft.alignment.center,
-        content=ft.Text("Projeto", size=18, color="black"),
+        alignment=ft.alignment.top_center,
+        content=ft.Text("PROJETOS", size=30, color=cores.AZUL_MARINHO_ESCURO, font_family="Roboto_black"),
     )
 
     # Colocando a imagem e o container lado a lado em um Row
     page.add(
         ft.Row(
             controls=[imagem_logo, rectangle_menu],
-            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,  # Ajusta o espaçamento entre os itens
-            vertical_alignment=ft.CrossAxisAlignment.CENTER,  # Centraliza os itens verticalmente
+            alignment=ft.MainAxisAlignment.CENTER,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            spacing=100,
         )
     )
 
