29a8487bd55444be954885b7e55996ee12b8b779
José Neto <140574691+jose-pcn@users.noreply.github.com>
2024-11-27 11:49:55 -0300

    Problema de centralização no fundo branco no arquivo create_project.py

--- pages/create_project.py
+++ pages/create_project.py
@@ -5,26 +5,27 @@ from assets import cores
 def create_project(page: ft.Page):
     # Limpando as pages para garantir que não há sobreposição
     page.views.clear()
-
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    rectangle_background = ft.Container(
+        width=850,
+        height=630,
+        bgcolor="white",
+        border_radius=20,
+        alignment=ft.alignment.top_center,
+        content=ft.Column(
+            controls=[
+                ft.Text("PROJETOS", size=30, color=cores.AZUL_MARINHO_ESCURO, font_family="Roboto_black")
+            ]
+        )
+    )
     # Adicionando uma nova view
     page.views.append(
         ft.View(
             "/create_project",
             bgcolor=cores.AZUL_MARINHO_ESCURO,
             controls=[
-                ft.Container(
-                    width=450,
-                    height=630,
-                    bgcolor="white",
-                    border_radius=20,
-                    alignment=ft.alignment.top_center,
-                    content=ft.Text(
-                        "PROJETO",
-                        size=30,
-                        color=cores.AZUL_MARINHO_ESCURO,
-                        font_family="Roboto_black"
-                    )
-                )
+                rectangle_background
             ]
         )
     )
